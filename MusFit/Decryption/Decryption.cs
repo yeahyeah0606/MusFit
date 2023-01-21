4ea3400948f403bc4b566d10b643bef1805d377f
Elaine1115 <e20051115@gmail.com>
2023-01-21 21:52:30 +0800

    2023.01.21_Login_AESencrypt

---
+++ MusFit/Decryption/Decryption.cs
@@ -0,0 +1,75 @@
+﻿using System.Text;
+using System;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace MusFit.EncryptPassword
+{
+    public class Decryption
+    {
+        public string DecryptStringAES(string encryptedValue)
+        {
+            var keybytes = Encoding.UTF8.GetBytes("7061737323313233");
+            var iv = Encoding.UTF8.GetBytes("7061737323313233");
+
+            //DECRYPT FROM CRIPTOJS
+            var encrypted = Convert.FromBase64String(encryptedValue);
+            var decriptedFromJavascript = DecryptStringFromBytes(encrypted, keybytes, iv);
+
+            return decriptedFromJavascript;
+        }
+
+        private static string DecryptStringFromBytes(byte[] cipherText, byte[] key, byte[] iv)
+        {
+            // Check arguments.
+            if (cipherText == null || cipherText.Length <= 0)
+            {
+                throw new ArgumentNullException("cipherText");
+            }
+            if (key == null || key.Length <= 0)
+            {
+                throw new ArgumentNullException("key");
+            }
+            if (iv == null || iv.Length <= 0)
+            {
+                throw new ArgumentNullException("key");
+            }
+
+            // Declare the string used to hold
+            // the decrypted text.
+            string plaintext = null;
+
+            // Create an RijndaelManaged object
+            // with the specified key and IV.
+            using (var rijAlg = new RijndaelManaged())
+            {
+                //Settings
+                rijAlg.Mode = CipherMode.CBC;
+                rijAlg.Padding = PaddingMode.PKCS7;
+                rijAlg.FeedbackSize = 128;
+
+                rijAlg.Key = key;
+                rijAlg.IV = iv;
+
+                // Create a decrytor to perform the stream transform.
+                var decryptor = rijAlg.CreateDecryptor(rijAlg.Key, rijAlg.IV);
+
+                // Create the streams used for decryption.
+                using (var msDecrypt = new MemoryStream(cipherText))
+                {
+                    using (var csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (var srDecrypt = new StreamReader(csDecrypt))
+                        {
+                            // Read the decrypted bytes from the decrypting stream
+                            // and place them in a string.
+                            plaintext = srDecrypt.ReadToEnd();
+                        }
+                    }
+                }
+            }
+
+            return plaintext;
+        }
+    }
+}

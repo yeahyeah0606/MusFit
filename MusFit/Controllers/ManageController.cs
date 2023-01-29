4325aaab722050f550258c714e5de8f159c3824a
jeff850524 <jeff850524@yahoo.com.tw>
2023-01-29 17:34:51 +0800

    修改css樣式

--- MusFit/Controllers/ManageController.cs
+++ MusFit/Controllers/ManageController.cs
@@ -516,7 +516,7 @@ namespace MusFit.Controllers
         public IActionResult EmployeeSearch(string ENumber, string EName, string EAccount)
         {
             var queryResult = (from e in _context.Employees
-                               where e.ENumber == ENumber || e.EName == EName || e.EAccount == EAccount
+                               where (e.ENumber == ENumber || e.EName == EName || e.EAccount == EAccount ) && e.EResignDate < DateTime.Now
                                select new EmployeeViewModel()
                                {
                                    ENumber = e.ENumber,

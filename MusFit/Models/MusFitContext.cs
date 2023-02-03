abe51cbbb175078e662803cee6056b1719cc92fb
rachel0407 <ann95219@gmail.com>
2023-02-03 11:52:02 +0800

    課程介紹半成品

--- MusFit/Models/MusFitContext.cs
+++ MusFit/Models/MusFitContext.cs
@@ -34,6 +34,7 @@ namespace MusFit.Models
         public virtual DbSet<VwCoachSchedule> VwCoachSchedules { get; set; }
         public virtual DbSet<VwInBody> VwInBodies { get; set; }
 
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.HasAnnotation("Relational:Collation", "Chinese_Taiwan_Stroke_CI_AS");
@@ -47,6 +48,8 @@ namespace MusFit.Models
                 entity.Property(e => e.CId).HasColumnName("cID");
 
                 entity.Property(e => e.CActual).HasColumnName("cActual");
+
+                entity.Property(e => e.CExict).HasColumnName("cExict");
 
                 entity.Property(e => e.CExpect).HasColumnName("cExpect");
 
@@ -745,7 +748,7 @@ namespace MusFit.Models
             modelBuilder.Entity<Term>(entity =>
             {
                 entity.HasKey(e => e.TId)
-                    .HasName("PK__Term__DC1157077EF9CCFC");
+                    .HasName("PK__Term__DC1157076E6EABA1");
 
                 entity.ToTable("Term");
 
